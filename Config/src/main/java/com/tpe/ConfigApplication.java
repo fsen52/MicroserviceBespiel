3f896a5c44edd4c2ee7aa8c6fd1d695b23d2cea0
fsen52 <furkansen.52@gmail.com>
2023-08-07 19:21:13 +0200

    first commit

---
+++ Config/src/main/java/com/tpe/ConfigApplication.java
@@ -0,0 +1,20 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+//Das ist ein Config server
+@EnableConfigServer
+
+//Das ist ein discorevable service
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
